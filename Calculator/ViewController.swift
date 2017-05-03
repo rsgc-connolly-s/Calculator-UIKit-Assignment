4b612bab16d6f12b4ac5a48d2a391ade4b3e07cd
Sean <student@macbook-pro-405-connolly.local>
2017-05-03 11:21:07 -0400

    Linked buttons up two four

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //this is the connection to the lable in out view
+    @IBOutlet weak var lableDisplay: UILabel!
+    
+    // Create and instance (object) of the Calculator class
+    var model = Calculator ()
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onePressed(_ sender: Any) {
+        model.addToNewValue(digit: "1")
+        lableDisplay.text = model.providedValue
+    }
 
+    @IBAction func twoPressed(_ sender: Any) {
+        model.addToNewValue(digit: "2")
+        lableDisplay.text = model.providedValue
+    }
+    @IBAction func threePressed(_ sender: Any) {
+        model.addToNewValue(digit: "3")
+        lableDisplay.text = model.providedValue
+    }
+    @IBAction func fourPressed(_ sender: Any) {
+        model.addToNewValue(digit: "4")
+        lableDisplay.text = model.providedValue
+    }
 }
 
